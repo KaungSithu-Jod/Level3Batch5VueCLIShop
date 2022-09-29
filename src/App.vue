06d7b6d35aa9a247d5fbe980990ed5e61148fce9
KaungSithu-Jod <kaungsithu.lashio@gmail.com>
2022-09-29 18:23:08 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container px-4 px-lg-5">
+      <router-link class="navbar-brand" to="/">Start Bootstrap</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span
+          class="navbar-toggler-icon"></span></button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-4">
+          <li class="nav-item"><router-link class="nav-link active" aria-current="page" to="/">Home</router-link></li>
+          <li class="nav-item"><router-link class="nav-link" to="/about">About</router-link></li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" id="navbarDropdown" to="#" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">Shop</a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><router-link class="dropdown-item" to="/shop">All Products</router-link></li>
+              <li>
+                <hr class="dropdown-divider" />
+              </li>
+              <li><router-link class="dropdown-item" to="/shop">Popular Items</router-link></li>
+              <li><router-link class="dropdown-item" to="/shop">New Arrivals</router-link></li>
+            </ul>
+          </li>
+        </ul>
+        <form class="d-flex">
+          <button class="btn btn-outline-dark" type="submit">
+            <i class="bi-cart-fill me-1"></i>
+            Cart
+            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+          </button>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <router-view />
+
+  <footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2022</p>
+    </div>
+  </footer>
+</template>
+  
+
+<style src="@/assets/css/styles.css">
+
+</style>
