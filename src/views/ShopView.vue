15c95ddb3adc1ef74d3298bcba0c0894e3832a64
KaungSithu-Jod <kaungsithu.lashio@gmail.com>
2022-09-29 19:05:08 +0630

    Card.vue added

--- src/views/ShopView.vue
+++ src/views/ShopView.vue
@@ -6,24 +6,7 @@
         <div class="container px-4 px-lg-5 mt-5">
             <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
                 <div class="col mb-5" v-for="(row,index) in products" :key="index">
-                    <div class="card h-100">
-                        <!-- Product image-->
-                        <img class="card-img-top" :src="row.image" alt="..." />
-                        <!-- Product details-->
-                        <div class="card-body p-4">
-                            <div class="text-center">
-                                <!-- Product name-->
-                                <h5 class="fw-bolder">{{row.title}}</h5>
-                                <!-- Product price-->
-                                {{row.price}}
-                            </div>
-                        </div>
-                        <!-- Product actions-->
-                        <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                            <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">View
-                                    options</a></div>
-                        </div>
-                    </div>
+                    <Card :product="row"></Card>
                 </div>
             </div>
         </div>
@@ -35,9 +18,11 @@
     import Header from '@/components/Header.vue';
     import axios from "axios";
 
+    import Card from '@/components/Card.vue';
+
 export default {
     name: 'ShopView',
-    components: {Header},
+    components: { Header, Card },
     data() {
         return {
             products: [],
